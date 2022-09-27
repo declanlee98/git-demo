9c948c6ed5b2753458dad9b42fec1f6b14d6251f
Declan Ying Hui Lee <Declan.Lee53@thehutgroup.com>
2022-09-27 13:47:15 +0100

    added mars function

--- hello2.py
+++ hello2.py
@@ -1,6 +1,13 @@
+import sys
 
 def hello():
-	print("Hello, World")
-if __name__ == "__main__":
-	hello()
+	if sys.argv[1] == "mars":
+		hellomars()
+	else:
+		helloworld()
 
+def hellomars():
+	print("Hello, Mars")
+
+def helloworld():
+	print("Hello, World")
