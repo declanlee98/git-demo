22a464ef01dcea82f65371e87d541be482d9a535
Declan Ying Hui Lee <Declan.Lee53@thehutgroup.com>
2022-09-27 13:54:18 +0100

    added jupiter function

--- hello2.py
+++ hello2.py
@@ -1,6 +1,17 @@
-
+import sys 
 def hello():
+	if sys.argv[1] == "Jupiter":
+		hellojupiter()
+
+def hellojupiter():
+	print("Hello, Jupiter")
+
+def helloworld():
 	print("Hello, World")
+
+def hellojupiter():
+	print("Hello, Jupiter")
+
 if __name__ == "__main__":
 	hello()
 
